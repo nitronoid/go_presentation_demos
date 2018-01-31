b833542942e99a83d88c98a2954abb69d9cf2949
nitronoid <jackdiver@hotmail.co.uk>
2018-01-31 18:26:49 +0000

    Finishing up package demo

--- pkg_demo.go
+++ pkg_demo.go
@@ -6,5 +6,21 @@ import (
 	)
 
 func main() {
-	fmt.Println(pkgDemo.PublicFunc())
+
+	// Exported functions
+	fmt.Println(pkgDemo.Exported())
+	fmt.Println(pkgDemo.Exported2())
+
+	// We can only use named intialisation with exported members
+	a := pkgDemo.ExportedType{ExportedMember: 50}
+
+	// We can't construct this member even though it is exported,
+	// this is because it's type isn't exported
+	b := pkgDemo.ExportedType2{}
+
+	// We still get access to the member though
+	fmt.Println(b.X)
+
+	fmt.Println(a, b)
+
 }
--- pkg_demos/a_pkg.go
+++ pkg_demos/a_pkg.go
@@ -1,5 +1,19 @@
 package pkg_demos
 
-func PublicFunc () string {
-	return "Hello World"
+func Exported() string {
+	return "This is an Exported function!"
 }
+
+func Exported2() string {
+	return "This is an Exported function calling " + nonExported()
+}
+
+func nonExported() string {
+	return "a non-Exported function!"
+}
+
+type ExportedType struct {ExportedMember, nonExportedMember int}
+
+type ExportedType2 struct {X nonExportedType}
+
+type nonExportedType struct {x int}
